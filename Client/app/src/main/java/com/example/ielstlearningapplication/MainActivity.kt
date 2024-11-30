58f21438c3a6a517835a5d0b34c0e0d5112bd41e
Hoanghuygia <hoanghuy051230@gmail.com>
2024-11-30 15:35:54 +0700

    base code for client and splash creen set up

---
+++ Client/app/src/main/java/com/example/ielstlearningapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ielstlearningapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.ielstlearningapplication.ui.theme.IELsTLearningApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+}
