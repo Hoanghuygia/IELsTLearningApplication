65ad7a72192c3743356692f16fed04ee26b0aabb
MrT2008 <haitrieu6212@gmail.com>
2024-12-18 22:08:21 +0700

    fix structure

--- Client/app/src/main/java/com/example/ielstlearningapplication/presentation/pages/Speaking/LessonScreen.kt
+++ Client/app/src/main/java/com/example/ielstlearningapplication/presentation/pages/Speaking/LessonScreen.kt
@@ -16,9 +16,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,7 +26,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.ielstlearningapplication.R
 import com.example.ielstlearningapplication.ui.theme.IELsTLearningApplicationTheme
-
 
 @Composable
 fun LessonScreen(navController: NavHostController) {
@@ -41,177 +40,148 @@ fun LessonScreen(navController: NavHostController) {
                             contentDescription = "Back"
                         )
                     }
-
                 },
                 backgroundColor = Color.White
             )
-        },
-    )
-        { paddingValues ->
-            Column(
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            // Header Section
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-//                    .padding(16.dp)
-                    .padding(paddingValues),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentAlignment = Alignment.TopCenter
             ) {
                 Column(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceBetween
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Spacer(modifier = Modifier.height(16.dp))
-                    //question number
                     Text(
                         text = "0/10",
                         color = Color(0xFFA9A9A9),
                         fontSize = 18.sp,
                         fontFamily = FontFamily.Default,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                        textAlign = TextAlign.Center,
                     )
-                    //question
-                    Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.height(15.dp))
                     Text(
-                        text = "What is your favorite hobby?",
+                        text = "1. What is your favorite hobby?",
                         fontSize = 32.sp,
                         fontFamily = FontFamily.Default,
                         color = Color.Black,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
                     )
-                    //recording frame
-//                    Spacer(modifier = Modifier.weight(1f))
-                    Spacer(modifier = Modifier.height(200.dp))
-                    Column(
+                }
+            }
+
+            // Main Content Section
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp) // Tổng chiều cao của container
+            ) {
+                // Box chứa thời gian và các nút
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .background(Color(0xFFE8E9EB), RoundedCornerShape(25.dp)),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    Text(
+                        text = "0:00",
+                        fontSize = 20.sp,
+                        color = Color(0xFF002147),
                         modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween
-                    ){
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(100.dp)
-                                .background(Color(0xFFE8E9EB), RoundedCornerShape(25.dp)),
-                            contentAlignment = Alignment.BottomCenter
-                        ) {
-                            Text(
-                                text = "0:00",
-                                fontSize = 20.sp,
-                                color = Color(0xFF002147),
-                                modifier = Modifier
-                                    .padding(top = 8.dp)
-                                    .align(Alignment.TopCenter)
-                            )
-                            // Placeholder for waveform
-                        }
-                        //buttons
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .height(68.dp)
-                                .padding(0.dp),
-//                            .background(painter = painterResource(id = R.drawable.button_background), contentScale = ContentScale.Crop)
-                            contentAlignment = Alignment.BottomCenter
-                        ) {
-//                        Box(
-//                            modifier = Modifier
-//                                .fillMaxSize()
-//                                .height(68.dp)
-//                                .padding(0.dp),
-//                            contentAlignment = Alignment.CenterHorizontally
-//                        ){
-//
-//                        }
+                            .padding(top = 16.dp)
+                    )
+                }
 
-                            Image(
-                                painter = painterResource(id = R.drawable.button_background), // Thay bằng hình ảnh nền của bạn
+                // Nút điều khiển
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 20.dp)
+                        .align(Alignment.BottomCenter),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.button_background),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(68.dp)
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        // Nút tròn màu trắng (Stop)
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .background(Color.White, RoundedCornerShape(30.dp)),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Stop,
                                 contentDescription = null,
-                                modifier = Modifier
-                                    .height(68.dp)
-                                    .padding(horizontal = 0.dp)
-                                    .align(Alignment.BottomCenter)
+                                tint = Color(0xFFF42F4C),
+                                modifier = Modifier.size(30.dp)
                             )
-                            Row(
-                                modifier = Modifier
-                                    .align(Alignment.BottomCenter)
-                                    .padding(horizontal = 0.dp)
-                                    .height(68.dp)
-                                    .align(Alignment.Center)
-                            ) {
-                                Spacer(modifier = Modifier.width(5.dp))
+                        }
 
-                                // Phần bên trái: nút tròn
-                                Box(
-                                    modifier = Modifier
-                                        .size(60.dp)
-                                        .background(Color.White, shape = RoundedCornerShape(34.dp)),
-                                    contentAlignment = Alignment.Center
+                        // Nút lùi
+                        IconButton(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .background(Color.White, shape = RoundedCornerShape(topStart = 50.dp, bottomStart = 50.dp))
+                                .width(100.dp)
+                                .height(60.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = null,
+                                tint = Color(0xFFD9D9D9),
+                                modifier = Modifier.size(22.dp)
+                            )
+                        }
 
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Default.Stop, // Thay bằng icon màu đỏ của bạn
-                                        contentDescription = null,
-                                        tint = Color(0xFFF42F4C),
-                                        modifier = Modifier.size(30.dp)
-                                    )
-                                }
-
-                                Spacer(modifier = Modifier.weight(1f))
-
-                                // Phần bên phải: nút tiến và lùi
-                                Row {
-
-                                    IconButton(
-                                        onClick = { /*TODO*/ },
-                                        modifier = Modifier
-                                            .background(Color.White, shape = RoundedCornerShape(topStart = 50.dp, bottomStart = 50.dp))
-                                            .width(100.dp)
-                                            .height(60.dp)
-                                    ) {
-                                        Icon(
-                                            imageVector = Icons.Default.ArrowBack,
-                                            contentDescription = null,
-                                            tint = Color(0xFFD9D9D9),
-                                            modifier = Modifier.size(22.dp)
-                                        )
-                                    }
-
-                                    Spacer(modifier = Modifier.width(5.dp))
-
-                                    IconButton(
-                                        onClick = { /*TODO*/ },
-                                        modifier = Modifier
-                                            .background(Color.White, shape = RoundedCornerShape(topEnd = 50.dp, bottomEnd = 50.dp))
-                                            .width(100.dp)
-                                            .height(60.dp)
-
-                                    ) {
-                                        Icon(
-                                            imageVector = Icons.Default.ArrowForward,
-                                            contentDescription = null,
-                                            tint = Color(0xFF002147),
-                                            modifier = Modifier.size(22.dp)
-                                        )
-                                    }
-                                }
-                                Spacer(modifier = Modifier.width(5.dp))
-                            }
+                        // Nút tiến
+                        IconButton(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .background(Color.White, shape = RoundedCornerShape(topEnd = 50.dp, bottomEnd = 50.dp))
+                                .width(100.dp)
+                                .height(60.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowForward,
+                                contentDescription = null,
+                                tint = Color(0xFF002147),
+                                modifier = Modifier.size(22.dp)
+                            )
                         }
                     }
-
-
                 }
             }
         }
-
-
-
-
+    }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewLessonScreen() {
